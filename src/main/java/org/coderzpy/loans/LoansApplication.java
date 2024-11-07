7e29350ca321b0fb8a8e8b809fd9710d246a3bfa
Hassan Raza <mhraza95@gmail.com>
2024-11-07 13:45:17 +0530

    MySQL DB config

--- src/main/java/org/coderzpy/loans/LoansApplication.java
+++ src/main/java/org/coderzpy/loans/LoansApplication.java
@@ -4,8 +4,10 @@ import org.coderzpy.loans.dto.LoansContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @EnableConfigurationProperties(value = {LoansContactInfoDto.class})
 public class LoansApplication {
 
